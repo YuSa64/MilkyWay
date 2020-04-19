5c7184d0347db157531e38984f57959670cee0af
YuSa <rtgh64@gmail.com>
2020-04-19 16:57:31 -0500

    Some GUI works

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,17 +1,45 @@
 package application;
 
+
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application{
+  
+  private static final int WINDOW_WIDTH = 1600;
+  private static final int WINDOW_HEIGHT = 900;
 
   @Override
-  public void start(Stage arg0) throws Exception {
-    // TODO Auto-generated method stub
+  public void start(Stage primaryStage) throws Exception {
+    BorderPane root = new BorderPane();
+    Scene mainScene = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT);
+    HBox topPanel = new HBox();
+    HBox centerPanel = new HBox();
+    VBox rightPanel = new VBox();
+    TableView<DailyWeight> csvTable = new TableView<>();
     
+    rightPanel.setPadding(new Insets(10));
+    rightPanel.setSpacing(8);
+    rightPanel.getChildren().add(new Text("Sample"));
+    
+    
+    root.setTop(topPanel);
+    centerPanel.getChildren().add(csvTable);
+    centerPanel.getChildren().add(rightPanel);
+    root.setCenter(centerPanel);
+    primaryStage.setTitle("Milky Way");
+    primaryStage.setScene(mainScene);
+    primaryStage.show();
   }
 
-  public void main(String[] args) {
+  public static void main(String[] args) {
     launch(args);
   }
 }
