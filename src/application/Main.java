770b192ab868c43ff3e314496bbf4c96faa25606
YuSa <rtgh64@gmail.com>
2020-04-19 17:12:01 -0500

    Update Main.java

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,7 +4,10 @@ package application;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -23,16 +26,31 @@ public class Main extends Application{
     HBox topPanel = new HBox();
     HBox centerPanel = new HBox();
     VBox rightPanel = new VBox();
-    TableView<DailyWeight> csvTable = new TableView<>();
+    TableView csvTable = new TableView();
+    
+    root.setPadding(new Insets(10));
+    
+    topPanel.setPadding(new Insets(10));
+    topPanel.setSpacing(10);
+    topPanel.getChildren().addAll(new Button("DATA"), new Button("FARM"), new Button("ANNUAL"), new Button("MONTHLY"), new Button("RANGE"));
     
     rightPanel.setPadding(new Insets(10));
-    rightPanel.setSpacing(8);
+    rightPanel.setSpacing(10);
     rightPanel.getChildren().add(new Text("Sample"));
     
+    centerPanel.setPadding(new Insets(10));
+    centerPanel.setSpacing(10);
+    centerPanel.getChildren().addAll(csvTable, rightPanel); 
+    
+    TableColumn f1 = new TableColumn("FARM");
+    f1.setCellValueFactory(new PropertyValueFactory<>("farm"));
+    TableColumn f2 = new TableColumn("DATE");
+    f1.setCellValueFactory(new PropertyValueFactory<>("date"));
+    TableColumn f3 = new TableColumn("WEIGHT");
+    f1.setCellValueFactory(new PropertyValueFactory<>("weight"));
+    csvTable.getColumns().addAll(f1, f2, f3);
     
     root.setTop(topPanel);
-    centerPanel.getChildren().add(csvTable);
-    centerPanel.getChildren().add(rightPanel);
     root.setCenter(centerPanel);
     primaryStage.setTitle("Milky Way");
     primaryStage.setScene(mainScene);
